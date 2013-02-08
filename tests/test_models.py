e3f35ab383329067aebd99d283cb9341b7cdf26d
Alex Gaynor <alex.gaynor@rd.io>
2013-02-07 23:33:43 -0800

    get stuff working

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,30 @@
+import datetime
+
+from topaz_site.models import Build
+
+
 class TestModels(object):
+    def create_build(self, models):
+        sha1 = "a" * 40
+        platform = "osx"
+        success = True
+        timestamp = datetime.datetime.utcnow()
+        with models.engine.connect() as conn:
+            result = conn.execute(models.builds.insert().values(
+                sha1=sha1,
+                platform=platform,
+                success=success,
+                timestamp=timestamp
+            ))
+        [id] = result.inserted_primary_key
+        return Build(
+            id=id, sha1=sha1, platform=platform, success=success,
+            timestamp=timestamp
+        )
+
     def test_get_builds(self, models):
         orig_build = self.create_build(models)
-        builds = models.get_buidls()
+        builds = models.get_builds()
         assert len(builds) == 1
         [build] = builds
         assert build.id == orig_build.id
